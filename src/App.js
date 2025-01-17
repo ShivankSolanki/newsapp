eed3b22d3efb079239a7994865b60ca51e891eb3
ShivankSolanki <shivank1972@gmail.com>
2025-01-17 19:51:11 +0530

    more minor stuff, and setting up diff componants

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 // ApiKey : a9d5947216294a3bb2b897c52168e35d
 
 import './App.css';
+import Navbar from './componants/Navbar'
+import News from './componants/News'
 
 import React, { Component } from 'react'
 
 export default class App extends Component {
   render() {
     return (
-      <div style={{backgroundColor : '#141414'}}>
+      <div>
+        <Navbar/>
+        <News/>
         
       </div>
     )
